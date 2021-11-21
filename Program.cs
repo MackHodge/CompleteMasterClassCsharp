011b0e370c006580301a18707a5c93ec2ba1c3ab
Mack Hodge <mack.hodge@helplinenl.onmicrosoft.com>
2021-11-21 14:54:17 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Nullable
+﻿using System;
+
+namespace Nullable
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test commit");
+        }
     }
-  }
 }
