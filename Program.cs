b5a2b87cf71e5f501c937799630d6c32528c2741
Mack Hodge <mack.hodge@helplinenl.onmicrosoft.com>
2021-11-21 17:03:53 +0100

    Nullable basic

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Nullable
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Test commit");
+            double? num1 = null;
+            double? num4 = 0.514;
+            double num8;
+            //int num2 = null; can not sign null to an integer 
+            num1 = num4;
+
+            //num8 = num1; // can not convert nullable to double
+
+            num8 = (double) num1;
+            //if num4 is empty enter the numer 123.312
+            num8 = num4 ?? 123.312;
+            num8 = num1 ?? 123.312;
+
+            Console.WriteLine(num8);
+
         }
     }
 }
